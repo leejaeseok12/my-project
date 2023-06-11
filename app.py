9a731161674181e7dd69c9621bba8e6ca838e169
leejaeseok12 <135727132+leejaeseok12@users.noreply.github.com>
2023-06-12 01:25:29 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import control as ctrl
+from scipy import signal
 import streamlit as st
-view = [50,100,500]
-st.write('# sdsd')
-st.write('## zzzzzz')
-view
-st.bar_chart(view)
-import pandas as pd
-sview = pd.Series(view)
-sview
+
+# 전달 함수 정의
+num = [100]
+den = [1, 5, 6]
+system = ctrl.tf(num, den)
+
+# step response
+t, y = ctrl.step_response(system)
+st.line_chart(y)
+
+# Bode plot
+w, mag, phase = ctrl.bode(system, dB=True, Plot=False)
+st.line_chart(mag)
+st.line_chart(phase)
