d7c1885e1d83dc7e56e7f76e68313274a3e3a89e
leejaeseok12 <135727132+leejaeseok12@users.noreply.github.com>
2023-06-12 20:03:17 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -44,3 +44,56 @@ ax2_phase.semilogx(w, phase)
 ax2_phase.set_xlabel('Frequency [rad/s]')
 ax2_phase.set_ylabel('Phase [degrees]')
 st.pyplot(fig2)
+
+if st.button("app.py 코드 보기"):
+    code = '''
+    import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+st.set_page_config(
+    page_icon="🐶",
+    page_title="이자이스기 페이지",
+)
+
+# 전달함수 정의
+s1 = signal.lti([100], [1, 5, 106])
+
+# 주파수 범위 설정
+frequencies = np.logspace(-2, 2, 500)
+
+# 전달함수 그래프 계산
+t, y = signal.step(s1)
+
+# 주파수 응답 계산
+w, mag, phase = s1.bode(frequencies)
+
+# Streamlit 앱 구성
+st.title('202021052 이재석')
+st.subheader('폐루프 전달함수')
+st.write('100/(s^2 + 5s + 106)')
+
+# 전달함수 그래프
+st.header("Transfer Function Response")
+fig1, ax1 = plt.subplots()
+t, y, _ = signal.lsim(s1, np.ones_like(t), t)
+ax1.plot(t, y)
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Output')
+st.pyplot(fig1)
+
+# Bode Plot
+st.header("Bode Plot")
+fig2, (ax2_mag, ax2_phase) = plt.subplots(2, 1)
+ax2_mag.semilogx(w, mag)
+ax2_mag.set_ylabel('Magnitude [dB]')
+ax2_mag.set_title('Bode plot of G(s) = 100 / (s^2 + 5s + 106)')
+ax2_phase.semilogx(w, phase)
+ax2_phase.set_xlabel('Frequency [rad/s]')
+ax2_phase.set_ylabel('Phase [degrees]')
+st.pyplot(fig2)
+
+    '''
+
+    st.code(code, language="python")
